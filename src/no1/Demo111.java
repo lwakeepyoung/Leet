e980a9e6ea8274030fb80c00b6b17fd276e388f5
lwa <1183892495@qq.com>
2021-10-28 00:00:11 +0800

    1

---
+++ src/no1/Demo111.java
@@ -0,0 +1,22 @@
+package no1;
+
+import common.TreeNode;
+
+/**
+ * @Author: lwa
+ * @Date: 2021/10/27 22:56
+ */
+public class Demo111 {
+    public int minDepth(TreeNode root) {
+        if(root==null) {
+            return 0;
+        }
+        int height1 = minDepth(root.left);
+        int height2 = minDepth(root.right);
+        if(root.left==null || root.right==null){
+           return height1+height2+1;
+        }
+        return Math.min(height1,height2)+1;
+    }
+
+}
