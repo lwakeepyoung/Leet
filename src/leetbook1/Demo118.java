502359d1763513fdaef8bc4a55f39e43a289c073
liweiaomac <liweiao@doumob.com>
2021-12-10 18:44:03 +0800

    1

---
+++ src/leetbook1/Demo118.java
@@ -0,0 +1,16 @@
+package leetbook1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
