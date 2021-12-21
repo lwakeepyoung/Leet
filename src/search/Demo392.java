fd9983d50e2921f7f0efd6a8f6538395c8831bcf
liweiaomac <liweiao@doumob.com>
2021-12-21 18:49:25 +0800

    1

---
+++ src/search/Demo392.java
@@ -0,0 +1,11 @@
+package search;
+
+public class Demo392 {
+    public boolean isSubsequence(String s, String t) {
+        int sIndex=0;
+        int tIndex=0;
+        while (){
+
+        }
+    }
+}
